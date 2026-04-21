18f7c4b146439faaa20c579c69dcc2159f94bb5b
Ronald Yanevits <ryanevits@gmail.com>
2026-04-21 19:55:40 +0500

    Added a printed line about the purpose of the code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print('Hello, my first program!')
 
 
 # Code for add 2 numbers
+print('My first program add 2 numbers5')
 x = float(input('Enter first number : '))
 y = float(input('Enter second number : '))
 print(f'Sum = {x + y}')
